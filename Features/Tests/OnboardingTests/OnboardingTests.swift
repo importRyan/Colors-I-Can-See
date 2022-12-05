097fde977cb910cfa3d9cf06be98f4426a2c69de
Ryan Ferrell <ryan@inclusivityapp.com>
2022-12-05 15:56:50 -0800

    update test plans

--- Features/Tests/OnboardingTests/OnboardingTests.swift
+++ Features/Tests/OnboardingTests/OnboardingTests.swift
@@ -1,8 +1,27 @@
+import ComposableArchitecture
+import Models
 import XCTest
+
 @testable import Onboarding
 
 final class OnboardingTests: XCTestCase {
-  func testThisPasses() {
 
+  func testAcceptsInjectedVisionSimulation() {
+    let input = VisionType.tritan
+    let state = VisionTypes.State(vision: input)
+    XCTAssertNoDifference(input, state.vision)
+  }
+
+  func testEditsVisionSimulationSelection() {
+    let store = TestStore(
+      initialState: .init(vision: .tritan),
+      reducer: VisionTypes()
+    )
+    store.send(.binding(.set(\.$vision, .deutan))) {
+      $0.vision = .deutan
+    }
+    store.send(.binding(.set(\.$vision, .monochromat))) {
+      $0.vision = .monochromat
+    }
   }
 }
